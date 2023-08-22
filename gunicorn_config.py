d1a77fd0624d46264bfabfbcf340e2e805b39f1d
Fedor Sulitskiy <72353528+fedorSulitskiy@users.noreply.github.com>
2023-08-22 02:00:59 +0100

    Dockerise success!!!

---
+++ gunicorn_config.py
@@ -0,0 +1,13 @@
+# Number of worker processes
+workers = 4  
+# Host and port to bind to
+bind = "0.0.0.0:8000"  
+# Request timeout in seconds
+timeout = 120 
+
+# Use the console as the error log destination
+errorlog = '-'
+# Use the console as the access log destination
+accesslog = '-'
+# Set the log level (options: debug, info, warning, error, critical)
+loglevel = 'info'
